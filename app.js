bbbae11b25c3e2a22c752799d5d8a16975d7b0e2
Ian Webster <ian@room77.com>
2013-09-26 10:45:34 -0700

    Add dummy ssl certs.  Support https configuration.

--- app.js
+++ app.js
@@ -1,22 +1,42 @@
 #!/usr/bin/env node
 
 var express = require('express')
+  , http = require('http')
+  , https = require('https')
+  , fs = require('fs')
   , path = require('path')
   , process = require('process')
+  , colors = require('colors')
   , autoreload = require('connect-autoreload')
 
 var args = require('optimist')
     .options('port', {
       alias: 'p',
       default: 60000,
+      describe: 'Port server listens on',
     })
     .options('exclude', {
       alias: 'e',
       default: '\\.sw[poax]$',   // vim extensions
+      describe: 'Regex matching files to exclude',
     })
     .options('regex_opts', {
       default: 'gim',
+      describe: 'Exclusion regex options',
     })
+    .options('https', {
+      default: false,
+      describe: 'Use https',
+    })
+    .options('key', {
+      default: 'dummy_ssl/key.pem',
+      describe: 'SSL key',
+    })
+    .options('cert', {
+      default: 'dummy_ssl/cert.pem',
+      describe: 'SSL cert',
+    })
+    .usage('Usage: autoreload directories_to_watch..')
     .demand(1)   // require at least one non-option arg
     .argv
 
@@ -29,8 +49,22 @@ var config = {
   watch_dirs: dirs.join(' '),  // TODO this will break for dirs with spaces.  fix this in connect-autoreload
   ignore_regex: ignore_regex,
 }
-console.log(config.watch_dirs)
+console.info('\nAdd to your page:');
+console.info('\n\t<script src="%s://localhost:%d/autoreload.js"></script>\n',
+            args.https ? 'https' : 'http', args.port);
 
 var app = module.exports = express();
 app.use(autoreload(config));
-app.listen(args.port, null);
+
+if (args.https) {
+  var options = {
+    key: fs.readFileSync(path.resolve(__dirname, args.key)),
+    cert: fs.readFileSync(path.resolve(__dirname, args.cert)),
+  }
+  https.createServer(options, app).listen(args.port);
+  console.warn('*** To use the above script, you must go to https://localhost:%d and whitelist the SSL certificate, or provide your own with the --key and --cert options.\n'.red, args.port);
+} else {
+  app.listen(args.port, null);
+}
+
+console.info('Monitoring for changes:', config.watch_dirs)
